3bb656a6431a44b56f6068eebff62c4ce121398f
Nikita Goncharov <goncharovnikita.dev@gmail.com>
2022-02-17 09:53:11 +0300

    parse all body tags

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -8,7 +8,9 @@ import Data.Char (isUpper)
 import Html.Attribute
 import Html.Style
 import Html.HeadTags
+import Html.BodyTags
 import Html.Head (Head(Head))
+import Html.Body (Body(Body))
 
 tt = execTemplateParserForResult
 
@@ -130,4 +132,104 @@ spec = describe "ParserSpec" $ do
                     StyleElement ".div" [StyleProperty ("font-size", "12px")]
                 ]
             ])
+    
+
+    -- Body parsers
+    describe "Body parsers" $ do
+        it "should parse PlainText" $
+            tt parseBodyContentItem "\"hello\""
+            `shouldBe`
+            Right (PlainText "hello")
+        mapM_ (\(a, tag) -> do
+            let markup = mconcat [asText "[", tag, asText " ", asText "\"", tag, asText "\"", asText "\n]"]
+            it ("should parse " <> show tag <> " " <> show markup) $
+                tt parseBodyContentItem markup
+                `shouldBe`
+                Right (SectionTag a [] [PlainText tag])
+            ) [
+                (Article, asText "article"),
+                (Section, asText "section"),
+                (Nav, asText "nav"),
+                (Aside, asText "aside"),
+                (H1, asText "h1"),
+                (H2, asText "h2"),
+                (H3, asText "h3"),
+                (H4, asText "h4"),
+                (H5, asText "h5"),
+                (H6, asText "h6"),
+                (Hgroup, asText "hgroup"),
+                (Header, asText "header"),
+                (Footer, asText "footer"),
+                (Address, asText "address")
+            ]
+        mapM_ (\(a, tag) -> do
+            let markup = mconcat [asText "[", tag, asText " ", asText "\"", tag, asText "\"", asText "\n]"]
+            it ("should parse " <> show tag <> " " <> show markup) $
+                tt parseBodyContentItem markup
+                `shouldBe`
+                Right (GroupingTag a [] [PlainText tag])
+            ) [
+                (P, asText "p"),
+                (Hr, asText "hr"),
+                (Pre, asText "pre"),
+                (Blockquote, asText "blockquote"),
+                (Ol, asText "ol"),
+                (Ul, asText "ul"),
+                (Menu, asText "menu"),
+                (Li, asText "li"),
+                (Dl, asText "dl"),
+                (Dt, asText "dt"),
+                (Dd, asText "dd"),
+                (Figure, asText "figure"),
+                (Figcaption, asText "figcaption"),
+                (Main, asText "main"),
+                (Div, asText "div")
+            ]
+        mapM_ (\(a, tag) -> do
+            let markup = mconcat [asText "[", tag, asText " ", asText "\"", tag, asText "\"", asText "\n]"]
+            it ("should parse " <> show tag <> " " <> show markup) $
+                tt parseBodyContentItem markup
+                `shouldBe`
+                Right (TextTag a [] [PlainText tag])
+            ) [
+                (A, asText "a"),
+                (Em, asText "em"),
+                (Strong, asText "strong"),
+                (Small, asText "small"),
+                (S, asText "s"),
+                (Cite, asText "cite"),
+                (Q, asText "q"),
+                (Dfn, asText "dfn"),
+                (Abbr, asText "abbr"),
+                (Ruby, asText "ruby"),
+                (Rt, asText "rt"),
+                (Rp, asText "rp"),
+                (Data, asText "data"),
+                (Time, asText "time"),
+                (Code, asText "code"),
+                (Var, asText "var"),
+                (Samp, asText "samp"),
+                (Kbd, asText "kbd"),
+                (Sub, asText "sub"),
+                (Sup, asText "sup"),
+                (I, asText "i"),
+                (B, asText "b"),
+                (U, asText "u"),
+                (Mark, asText "mark"),
+                (Bdi, asText "bdi"),
+                (Bdo, asText "bdo"),
+                (Span, asText "span"),
+                (Br, asText "br"),
+                (Wbr, asText "wbr")
+            ]
+        mapM_ (\(a, tag) -> do
+            let markup = mconcat [asText "[", tag, asText " ", asText "\"", tag, asText "\"", asText "\n]"]
+            it ("should parse " <> show tag <> " " <> show markup) $
+                tt parseBodyContentItem markup
+                `shouldBe`
+                Right (EditTag a [] [PlainText tag])
+            ) [
+                (Ins, asText "ins"),
+                (Del, asText "del")
+            ]
 
